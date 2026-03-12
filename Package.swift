438433adb94a2b21910548d7888c2d5b93b69ff7
Alexey Grebennikov <60310478+ios-grebennikov@users.noreply.github.com>
2026-03-12 14:13:20 +0300

    Update Package.swift
    new checksum

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     targets: [
         .binaryTarget(
           name: "SwiftyXrayCore",
-          url: "https://github.com/dima-u/SwiftyXrayCore/releases/download/1.1.0/SwiftyXrayCore.xcframework.zip",
+          url: "https://github.com/ios-grebennikov/SwiftyXrayCore/releases/download/1.1.0/SwiftyXrayCore.xcframework.zip",
           checksum: "8cd20a604af0854c2ba18ad64135b33fd5843729b250813a497965dbef2f1275"
         )
     ]
